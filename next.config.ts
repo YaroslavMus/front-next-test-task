058daaec290161696193bb6369d2344374e51cb8
YaroslavMus <yaroslawmusato@gmail.com>
2025-11-25 14:56:32 +0300

    update config

--- next.config.ts
+++ next.config.ts
@@ -12,9 +12,8 @@ const nextConfig: NextConfig = {
       dangerouslyAllowLocalIP: isDev,
       remotePatterns: [
          {
-            protocol: 'http',
-            hostname: 'localhost',
-            port: '1337',
+            protocol: 'https',
+            hostname: 'backend-test-xe66.onrender.com',
             pathname: '/uploads/**'
          },
          {
