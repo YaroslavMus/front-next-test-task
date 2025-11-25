5001fd00c7187e7e9ee7e7d963f3eab722a31050
YaroslavMus <yaroslawmusato@gmail.com>
2025-11-25 15:03:41 +0300

    update config media

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { getHomeHero } from '@/lib/api/home'
 import { Home } from '@/componets/Home/Home'
 import '@/styles/index.scss'
 
-export const revalidate = 60 // или убери, если уже в fetch
 
 export default async function HomePage() {
   const hero = await getHomeHero()
